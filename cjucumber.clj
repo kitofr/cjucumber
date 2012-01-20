511924702c805fb76750fd217311016d233345a7
Kristoffer Roupé <kristoffer.roupe@cint.com>
2012-01-20 12:50:04 +0100

    Some more functionallity

--- cjucumber.clj
+++ cjucumber.clj
@@ -1,11 +1,20 @@
-
 (defonce givens (atom {}))
 
 (defmacro given [regex args & body]
   `(let [call# '(~args ~@body)]
-    (swap! givens assoc ~(keyword (str regex)) (call#))))
+    (swap! givens assoc ~(keyword (str regex)) call#)))
 
-(macroexpand-1 '(given #"hej" [:a :b] (println "hej")))
+(given #"hej" {:a :b} (println "hej"))
+(given #"bu" {:a :b} (println "bu"))
+(given #"bu" {:a :b} (println "bu"))
+(println (str "Givens count: " (count (keys @givens))))
 
-(given #"hej" [:a :b] (println "hej"))
-(println (count (keys @givens)))
+(defn run-step [step]
+  (let [fun (get @givens (keyword step))]
+    (if fun
+      (eval (second (second fun)))
+      (println (str "[pending] Given " step)))))
+
+(run-step "bu")
+(run-step "hej")
+(run-step "kek")
