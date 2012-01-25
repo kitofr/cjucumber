5a94acbe985628d7a156ec58ea844db11955f4cd
Kristoffer Roupe <kristoffer.roupe@cint.com>
2012-01-25 16:59:08 +0100

    Need to figure out how to explode that list

--- cjucumber.clj
+++ cjucumber.clj
@@ -1,24 +1,4 @@
 (ns cjucumber)
-;(defonce givens (atom {}))
-
-;(defmacro given [regex args & body]
-;  `(let [call# '(~args ~@body)]
-;    (swap! givens assoc ~(keyword (str regex)) call#)))
-
-;(given #"hej" {:a :b} (println "hej"))
-;(given #"bu" {:a :b} (println "bu"))
-;(given #"bu" {:a :b} (println "bu"))
-;(println (str "Givens count: " (count (keys @givens))))
-
-;(defn run-step [step]
-;  (let [fun (get @givens (keyword step))]
-;    (if fun
-;      (eval (second (second fun)))
-;      (println (str "[pending] Given " step)))))
-
-;(run-step "bu")
-;(run-step "hej")
-;(run-step "kek")
 
 (defonce fns (atom {}))
 (defn keyword->symbol [kw]
@@ -30,6 +10,9 @@
 (defn key->regex [k]
   (re-pattern (name k)))
 
+(defn regex->key [r]
+  (keyword (str r)))
+
 (defn parse-args [args]
   (map keyword->symbol args))
 
@@ -37,20 +20,23 @@
   (let [args (parse-args arg)]
     `(fn [~@args] ~@body)))
 
-;(macroexpand-1 '(create-fn (:a :b) (println a b)))
-
-
 (defmacro given [regex args & body]
-  `(swap! fns assoc (keyword (str ~regex))
+  `(swap! fns assoc (regex->key ~regex))
          (create-fn ~args ~@body)))
-
-;(macroexpand '(given #"arg1" (:a) (println a)))
 
 (defn regexes [] (map key->regex (keys @fns)))
 
-(given #"arg (\w+)" (:a) (println a))
+(defn regex-and-args [step]
+  (map (fn [regex]
+         (let [matches (re-find regex step)]
+           (if matches
+           (list regex (drop 1 matches))
+             nil))) 
+       (regexes)))
 
 (defn run-step [step]
-  (let [fun (map #(re-find %1 step) (regexes))]
-  ((get @fns step) "1")))
+  (let [fun-n-args (first (regex-and-args step))]
+  ((get @fns (regex->key (first fun-n-args))) (flatten (rest fun-n-args)))))
 
+(given #"arg (\w+)" (:a) (println a))
+(given #"args (\d+) (\d+)" (:a :b) (println (str a "|" b)))
