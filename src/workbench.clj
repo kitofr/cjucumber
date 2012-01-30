ebb14e40b0d1eeb8caf31ba5ed78a29eef88c9b2
kitofr <kitofr@gmail.com>
2012-01-30 21:56:20 +0100

    Testing...

--- src/workbench.clj
+++ src/workbench.clj
@@ -1,8 +1,10 @@
 (ns cjucumber.workbench
   (:use [cjucumber.core]))
 
-(given #"arg (\w+)" (:a) (println a))
-(given #"args (\d+) (\d+)" (:a :b) (println (str "args: [" a "|" b "]")))
-
-(run-step "Given args 5 102")
+(given #"arg (\w+)" (:a) 
+       (assert (< 5 a)))
 (run-step "Given arg 50")
+
+(given #"args (\d+) (\d+)" (:a :b) (println (str "args: [" a "|" b "]")))
+(run-step "Given args 5 102")
+
