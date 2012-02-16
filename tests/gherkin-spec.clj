f4211dd138812400bb873571a1fc2f1e3c935806
kitofr <kitofr@gmail.com>
2012-02-16 22:10:06 +0100

    A bit on the way at least

--- tests/gherkin-spec.clj
+++ tests/gherkin-spec.clj
@@ -1,19 +1,23 @@
 (ns gerkin-spec
   (:use clojure.test))
 
+(defn split-lines [s]
+  (seq (.Split #"\r?\n" s)))
+
 (defn gherkin-parse [input]
-  { :feature "In order to be able to read in feature files I would like cjucumber to parse gherkin grammar correctly" })
+  (let [feature (second (re-find #"Feature: ([A-Za-z\s+]+)Scenario:" feature-file))]
+    { :feature feature }))
 
 
 (def feature-file 
-"Feature: In order to be able to read in feature files
-          I would like cjucumber to parse gherkin grammar correctly
+  "Feature: In order to be able to read in feature files
+  I would like cjucumber to parse gherkin grammar correctly
 
-Scenario: Scenarios can have a title
-Given there is a given
-When the gherkin parser reads it
-Then it parses given/when/then as separate steps
-")
+  Scenario: Scenarios can have a title
+  Given there is a given
+  When the gherkin parser reads it
+  Then it parses given/when/then as separate steps
+  ")
 
 (deftest feature-definition-should-be-first
          (let [content (gherkin-parse feature-file)]
