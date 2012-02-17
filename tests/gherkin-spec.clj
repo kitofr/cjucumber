52c27c1f9c763f4e704b62c7e51b1cb2c453d146
Kristoffer Roupé <kristoffer.roupe@cint.com>
2012-02-17 14:54:45 +0100

    Rudamentory remove-ws

--- tests/gherkin-spec.clj
+++ tests/gherkin-spec.clj
@@ -2,11 +2,16 @@
   (:use clojure.test))
 
 (defn split-lines [s]
-  (seq (.Split #"\r?\n" s)))
+  (seq (.split #"\r?\n" s)))
+
+(defn remove-ws [s]
+  (let [stripped (map second (re-seq #"(\w+\,*\!*)\r*\n*\t*\s*" s))
+        words (map #(str %1 " ") (drop-last stripped))]
+    (apply str (flatten `(~words ~(last stripped))))))
 
 (defn gherkin-parse [input]
-  (let [feature (second (re-find #"Feature: ([A-Za-z\s+]+)Scenario:" feature-file))]
-    { :feature feature }))
+  (let [feature (second (re-find #"Feature: ([A-Za-z\s+]+)Scenario:" input))]
+    { :feature (remove-ws feature) }))
 
 
 (def feature-file 
@@ -22,5 +27,8 @@
 (deftest feature-definition-should-be-first
          (let [content (gherkin-parse feature-file)]
            (is (= (content :feature) "In order to be able to read in feature files I would like cjucumber to parse gherkin grammar correctly"))))
+
+(deftest remove-ws-should-strip-wses
+         (is (= "all 4 one, one 4 all!" (remove-ws "all    4\n one, \t\r one 4 all!\n"))))
 
 (run-tests)
